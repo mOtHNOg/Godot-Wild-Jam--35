6e0873c6f0bd8c846c2ae0fcd9d953b613fbca84
e <emeth@vaults.ca>
2021-07-18 11:23:16 -0400

    label with sound works and replaced end screen labels with it

--- scripts/LabelWithSound.gd
+++ scripts/LabelWithSound.gd
@@ -1,2 +1,24 @@
+tool
 extends Label
 class_name LabelWithSound
+
+onready var sfx_text = $Text
+
+var previous_percent_visible: float = percent_visible
+
+const text_sound_time = 10
+var text_sound_time_left: int = 0
+
+func _physics_process(delta):
+	if previous_percent_visible != percent_visible:
+		
+		if text_sound_time_left == 0:
+			
+			sfx_text.pitch_scale = rand_range(0.667, 1.333)
+			sfx_text.play()
+			
+			text_sound_time_left = text_sound_time
+		else:
+			text_sound_time_left -= 1
+	
+	previous_percent_visible = percent_visible
